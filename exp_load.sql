2d02bceb649ddfb4cefc66bdbd37d3a55c28d7e4
Jackson Simmons <jacksonsimmons7580@gmail.com>
2026-03-13 15:49:43 -0400

    bugfixes load

--- exp_load.sql
+++ exp_load.sql
@@ -29,15 +29,15 @@ INSERT INTO connections (room1, room2) VALUES (6, 7);
 -- SIX SEVEN!!!
 INSERT INTO connections (room1, room2) VALUES (7, 8);
 
-INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (1,1,1,1,'yerp',1,1);
-INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (2,2,2,2,'mhm',2,2);
-INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (3,3,3,3,'asjldkf',3,3);
-INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (4,4,4,4,'ahga',4,4);
-INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (5,5,5,5,'help',5,5);
-INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (6,6,6,6,'hello',6,6);
-INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (7,7,7,7,'hi',7,7);
-INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (8,8,8,8,'yeppers',8,8);
-INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (9,9,9,9,'pbinq',9,9);
-INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (10,10,10,10,'pbj',10,10);
-INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (11,11,11,11,'hungry',11,11);
-INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (12,12,12,12,'zdxvbb',12,12);
+INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (1,1,NULL,NULL,'yerp',1,1);
+INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (2,2,NULL,NULL,'mhm',2,2);
+INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (3,3,NULL,NULL,'asjldkf',3,3);
+INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (4,4,NULL,NULL,'ahga',4,4);
+INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (5,5,NULL,NULL,'help',5,5);
+INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (6,6,NULL,NULL,'hello',6,6);
+INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (7,7,NULL,NULL,'hi',7,7);
+INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (8,8,NULL,NULL,'yeppers',8,8);
+INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (9,9,NULL,NULL,'pbinq',9,9);
+INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (10,10,NULL,NULL,'pbj',10,10);
+INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (11,11,NULL,NULL,'hungry',11,11);
+INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (12,12,NULL,NULL,'zdxvbb',12,12);
