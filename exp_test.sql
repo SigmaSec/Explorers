b09c9c268be4494cdb38806353f61f72068c487e
Jackson Simmons <jacksonsimmons7580@gmail.com>
2026-03-13 16:39:49 -0400

    Finished with exp_testing. I am done.

--- exp_test.sql
+++ exp_test.sql
@@ -1,29 +1,33 @@
 --ALL OF THESE SHOULD FAIL
 --IF THEY DO NOT FAIL, WE HAVE A PROBLEM.
-INSERT INTO rooms (roomID, name, description) VALUES (1,'Kitchen','hungry');
-INSERT INTO rooms (roomID, name, description) VALUES (1,'Hallway','hi');
-INSERT INTO rooms (roomID, name, description) VALUES (2, 'Kitchen');
-
+--Duplicate room primary key
+INSERT INTO rooms (roomID, name, description) VALUES (1, 'foo', 'bar');
+--Duplicate room name
+INSERT INTO rooms (roomID, name, description) VALUES (9, 'foo','bar');
+--Explorer primary key duplicate
 INSERT INTO explorers (expID, name, username, Room_ID) VALUES (1,'Gary','Lobster8841', 1);
-INSERT INTO explorers (expID, name, username, Room_ID) VALUES (1, 'Hi','NoxNostra', 2)
-INSERT INTO explorers (expID, name, username, Room_ID) VALUES (2, 'Bebop','TheLash', 1);
-INSERT INTO explorers (expID, name, username, Room_ID) VALUES (3, 'Bebop','Dynamo', 3);
-INSERT INTO explorers (expID, name, username, Room_ID) VALUES (4, 'Admiral','Dynamo',4);
+--Explorer name duplicate
+INSERT INTO explorers (expID, name, username, Room_ID) VALUES (3, 'Tired','AnotherUser',2);
+--room does not exist
+INSERT INTO explorers (expID, name, username, Room_ID) VALUES (10, 'Ghost','user',999);
 
-INSERT INTO npcs (npcID, type, roomID) VALUES (1, 'java',1);
-INSERT INTO npcs (npcID, type, roomID) VALUES (1, 'take', 2);
-INSERT INTO npcs (npcID, type, roomID) VALUES (2, 'take', 3);
-INSERT INTO npcs (npcID, type, roomID) VALUES (3, 'almost', 3);
+--NPC primary key duplicate
+INSERT INTO npcs (npcID, type, roomID) VALUES (1, 'Javascript', 2);
+--mabye not needed?
+INSERT INTO npcs (npcID, type, roomID) VALUES (3,3);
 
-INSERT INTO connections (room1, room2) VALUES (1, 2);
-INSERT INTO connections (room1, room2) VALUES (1, 3);
-INSERT INTO connections (room1, room2) VALUES (2, 3);
+--Duplicate room connection
+INSERT INTO connections (room1, room2) VALUES (1,2);
+--room connecting to itself
+INSERT INTO connections (room1, room2) VALUES (3,3);
 
+--Tresure primary key duplicate
 INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (1,1,NULL,NULL,'yerp',1,1);
-INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (1,1,1,NULL,'piza',1,1);
-INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (1,1,1,1,'hut',1,1);
-INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (2,NULL,2,2,'dominos',1,1);
-INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (1,1,NULL,NULL,'lobster',1,1);
-INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (1,2,NULL,NULL,'yerp',1,1);
-INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (1,2,NULL,NULL,'yerp',3,4);
-INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (1,1,NULL,NULL,'asdf',4,4);
+--Treasure value invalid
+INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (20,1,NULL,NULL,'badvalue',0,1);
+--Treasure weight invalid
+INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (21,1,NULL,NULL,'badweight',10,-1);
+--Treasure in two locations
+INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (22,1,1,NULL,'twoOwners',10,1);
+--Treasure with no location
+INSERT INTO treasures (tresID, expID, npcID, roomID, description, value, weight) VALUES (23,NULL,NULL,NULL,'nowhere',10,1);
