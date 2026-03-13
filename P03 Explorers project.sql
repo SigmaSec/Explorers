3f337a479c9bdf08ac849650a3901cf19c3b0113
Jackson Simmons <jacksonsimmons7580@gmail.com>
2026-03-13 14:06:33 -0400

    Finished Version(?)

--- P03 Explorers project.sql
+++ P03 Explorers project.sql
@@ -5,7 +5,9 @@ CREATE TABLE explorers (
     Room_ID number,
 
     CONSTRAINT explorers_pk PRIMARY KEY (expID),
-)
+    CONSTRAINT rooms_fk FOREIGN KEY (Room_ID) REFERENCES rooms(roomID),
+    CONSTRAINT explorers_name_ck UNIQUE (name)
+);
 
 CREATE TABLE npcs (
     npcID number,
@@ -13,11 +15,12 @@ CREATE TABLE npcs (
     roomID number,
 
     CONSTRAINT npcs_pk PRIMARY KEY (npcID),
-    CONSTRAINT
-)
+    CONSTRAINT roomID_fk FOREIGN KEY (roomID) REFERENCES rooms(roomID)
+);
 
-CREATE TABLE treasure (
+CREATE TABLE treasures (
     tresID number,
+    expID number,
     npcID number,
     roomID number,
     description VARCHAR2(255),
@@ -25,27 +28,33 @@ CREATE TABLE treasure (
     weight number,
 
     CONSTRAINT treasure_pk PRIMARY KEY (tresID),
-    FOREIGN KEY (expID),
-    REFERENCES explorers(expID),
-    FOREIGN KEY (npcID),
-    REFERENCES npc(npcID),
-    FOREIGN KEY (roomID),
-    REFERENCES room(roomID),
+    CONSTRAINT expID_fk FOREIGN KEY (expID) REFERENCES explorers(expID),
+    CONSTRAINT npcID_fk FOREIGN KEY (npcID) REFERENCES npcs(npcID),
+    CONSTRAINT roomID_fk FOREIGN KEY (roomID) REFERENCES rooms(roomID),
     CONSTRAINT treasure_value_ck CHECK (value > 0),
-    CONSTRAINT treasure_weight_ck CHECK (weight > 0)
+    CONSTRAINT treasure_weight_ck CHECK (weight >= 0),
+    CONSTRAINT has_location_ck CHECK (
+        (expID IS NOT NULL AND npcID IS NULL AND roomID IS NULL) OR 
+        (npcID IS NOT NULL AND expID IS NULL AND roomID IS NULL) OR
+        (roomID IS NOT NULL AND expID IS NULL AND npcID IS NULL)
+        )
 );
 
-CREATE TABLE room (
+CREATE TABLE rooms (
     roomID number,
     name VARCHAR2(50),
     description VARCHAR2(255),
 
-    CONSTRAINT room_roomID_pk PRIMARY KEY (roomID),
-    CONSTRAINT room_name_ck CHECK NOT NULL,
-    CONSTRAINT room_description_ck CHECK NOT NULL
+    CONSTRAINT rooms_roomID_pk PRIMARY KEY (roomID),
+    CONSTRAINT rooms_name_ck UNIQUE (name),
 );
 
-CREATE TABLE connection (
-    FOREIGN KEY room_
-)
+CREATE TABLE connections (
+    room1 number,
+    room2 number,
 
+    CONSTRAINT connections_rooms_pk PRIMARY KEY (room1, room2),
+    CONSTRAINT connections_room1_fk FOREIGN KEY (room1) REFERENCES rooms(roomID),
+    CONSTRAINT connections_room2_fk FOREIGN KEY (room2) REFERENCES rooms(roomID),
+    CONSTRAINT connections_rooms_ck CHECK (room1 != room2),
+);
