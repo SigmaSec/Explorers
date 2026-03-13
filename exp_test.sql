2ad30c2778bd66cf81c5bfa0dc74da6d22a3422b
Jackson Simmons <jacksonsimmons7580@gmail.com>
2026-03-13 16:14:00 -0400

    test started

--- exp_test.sql
+++ exp_test.sql
@@ -1,3 +1,7 @@
 --ALL OF THESE SHOULD FAIL
 --IF THEY DO NOT FAIL, WE HAVE A PROBLEM.
+INSERT INTO rooms (roomID, name, description) VALUES (1,'Kitchen','hungry');
+INSERT INTO rooms (roomID, name, description) VALUES (1,'Hallway','hi');
+INSERT INTO rooms (roomID, name, description) VALUES (2, 'Kitchen');
+
 INSERT INTO 
