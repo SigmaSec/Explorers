4d9ff7204842b8935c607ac840504e86ebaa62f8
Jackson Simmons <jacksonsimmons7580@gmail.com>
2026-03-13 16:18:00 -0400

    explorers done

--- exp_test.sql
+++ exp_test.sql
@@ -4,4 +4,8 @@ INSERT INTO rooms (roomID, name, description) VALUES (1,'Kitchen','hungry');
 INSERT INTO rooms (roomID, name, description) VALUES (1,'Hallway','hi');
 INSERT INTO rooms (roomID, name, description) VALUES (2, 'Kitchen');
 
-INSERT INTO 
+INSERT INTO explorers (expID, name, username, Room_ID) VALUES (1,'Gary','Lobster8841', 1);
+INSERT INTO explorers (expID, name, username, Room_ID) VALUES (1, 'Hi','NoxNostra', 2)
+INSERT INTO explorers (expID, name, username, Room_ID) VALUES (2, 'Bebop','TheLash', 1);
+INSERT INTO explorers (expID, name, username, Room_ID) VALUES (3, 'Bebop','Dynamo', 3);
+INSERT INTO explorers (expID, name, username, Room_ID) VALUES (4, 'Admiral','Dynamo',4);
