d08127a61e6c30bb1ba5c2874832804f16e92c2b
Jackson Simmons <jacksonsimmons7580@gmail.com>
2026-03-24 13:34:09 -0400

    thanks

--- Stored_procedures/create_npc_trader.sql
+++ Stored_procedures/create_npc_trader.sql
@@ -1,4 +1,5 @@
-CREATE PROCEDURE npc_trader(roomID VARCHAR2(255), rooms_name_ck VARCHAR2(255), room_description VARCHAR2(255)) AS npc_procedure
+CREATE PROCEDURE npc_trader(
+    roomID VARCHAR2(255), rooms_name_ck VARCHAR2(255), room_description VARCHAR2(255)) AS npc_procedure
 BEGIN
 SELECT room.roomID, room.name, room.description
 FROM room
